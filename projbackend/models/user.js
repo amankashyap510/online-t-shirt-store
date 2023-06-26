9691e2fca9e0f610d7303eaf20a56436ce68a53d
amankashyap510 <amankashyap510gmail.com>
2023-06-27 01:23:43 +0530

    basic model setup

---
+++ projbackend/models/user.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    userinfo: {
+        type: String,
+        trim: true
+    },
+    // TODO: come back
+    encry_password: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    purchases: {
+        type: Array,
+        default: []
+    }
+  });
+
+  module.exports = mongoose.model("User", userSchema)
