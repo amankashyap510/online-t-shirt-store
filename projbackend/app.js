46867a97a147b5c768fc58f6a53352f1da2eb873
amankashyap510 <amankashyap510gmail.com>
2023-06-30 17:01:15 +0530

    DB connection done

---
+++ projbackend/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+mongoose.connect("mongodb://127.0.0.1:27017/tshirt",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED")
+})
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`App is running at ${port}`)
+})
